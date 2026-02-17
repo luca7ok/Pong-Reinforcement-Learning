4425bd1e76b74482c22a8450de97a6e9d3b63d77
Tomsa Luca <lucasebastian777@gmail.com>
2026-02-17 17:04:44 +0200

    add constants for dqn

--- include/Constants.h
+++ include/Constants.h
@@ -44,5 +44,17 @@ namespace Constants {
     inline constexpr float MAX_BOUNCE_ANGLE{60.f * (M_PI / 180.f)};
     inline constexpr float MAX_LAUNCH_ANGLE{25.f * (M_PI / 180.f)};
 
+    inline constexpr int MAX_BUFFER_CAPACITY{10000};
+    inline constexpr int BATCH_SIZE{64};
+    inline constexpr float LEARNING_RATE{1e-3};
+    inline constexpr float GAMMA{0.99f};
+    inline constexpr float EPSILON{1.f};
+    inline constexpr float EPSILON_DECAY{0.95f};
+    inline constexpr float EPSILON_MIN{0.01f};
+    inline constexpr int TARGET_UPDATE_FREQ{1000};
+    inline constexpr int INPUT_LAYER_SIZE{6};
+    inline constexpr int HIDDEN_LAYER_SIZE{128};
+    inline constexpr int OUTPUT_LAYER_SIZE{3};
+
     enum class CollisionType { None, Top, Paddle, Bottom, Left, Right };
 }  // namespace Constants
